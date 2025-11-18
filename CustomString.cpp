e2f856e7c4859ecccc4356e4be407a9dbac86b0b
Andrew Emad <andrew14emad@gmail.com>
2025-11-19 00:46:25 +0200

    string functions

---
+++ CustomString.cpp
@@ -0,0 +1,92 @@
+#include "CustomString.h"
+#include <iostream>
+#define DEFAULT_VAL 0
+
+
+                                            //returns char arr size
+int str_length(char arr[]){
+    int target_size=DEFAULT_VAL;
+    while(*(arr)){
+        arr++;
+        target_size++;
+
+    }
+    return target_size;
+
+
+}
+
+int str_compare(char str1[],char str2[]){
+    int sumStr1=DEFAULT_VAL,sumStr2=DEFAULT_VAL;
+    while(*(str1)||*(str2)){
+        if(*(str1)){
+            str1++;
+            sumStr1+=*str1;
+        }
+        if(*(str2)){
+            str2++;
+            sumStr2+=*str2;
+        }
+    }
+    if(sumStr1>sumStr2){
+            return -1;
+
+    }else if(sumStr1<sumStr2){
+        return 1;
+    }else{
+        return 0;
+    }
+
+
+}
+
+
+bool toLower(char str[]){
+    int diff='a'-'A';
+    while(*(str)){
+        if(*str>='A'&&*str<='Z'){
+            *str+=diff;
+        }
+        str++;
+    }
+    return 1;
+}
+
+bool toUpper(char str[]){
+    int diff='a'-'A';
+    while(*(str)){
+        if(*str>='a'&&*str<='z'){
+            *str-=diff;
+        }
+        str++;
+    }
+    return 1;
+}
+bool str_concatinate(char str1[],char str2[],int size_str1){
+    int str1Length=str_length(str1),str2Length=str_length(str2);
+    if(str1Length+str2Length>size_str1)return 0;
+
+    str1+=str1Length;
+
+    while(*(str2)){
+
+        *(str1)=*(str2);
+
+        str1++;
+        str2++;
+    }
+    return 1;
+}
+
+bool str_copy(char str1[],char str2[],int size_str2){
+    int str1Length=str_length(str1);
+    if(str1Length>size_str2)return 0;
+    while(*(str1)){
+        *(str2)=*(str1);
+        str1++;
+        str2++;
+
+    }
+    return 1;
+
+}
--- main.cpp
+++ main.cpp
@@ -1,70 +1,14 @@
 #include <iostream>
-#include "TerminalModifier.h"
-#define DEFAULT_VAL 0
-#define STARTNUM 1
-#define STARTROW 0
-#define PADDING 5
-#define OFFSET 10
-#define DEFAULT_STRING_VAL ""
+#include "CustomString.h"
+
 using namespace std;
 
 
 int main(void) {
-    TerminalModifier myTerm;
-
-
-    int magicBox=DEFAULT_VAL;
-    string currentColor= DEFAULT_STRING_VAL;
-    currentColor=myTerm.getRandColor();
-    int exit=DEFAULT_VAL;
-    while(!exit){
-          myTerm.displayText(YELLOW_COLOR,"Please choose an odd number:",0,0);
-          cin>>magicBox;
-          if(magicBox%2==0){
-                myTerm.displayText(MAGENTA_COLOR,"this isnt an odd number, please choose an odd one",0,0);
-                myTerm.terminalSleep(1);
-                myTerm.clearScreen();
-          }else{
-              exit=1;
-          }
-
-
-
-    }
-
-
-    int num = STARTNUM;
-    int row = STARTROW;                  // Start at top row
-    int col = magicBox / 2;              // Middle column
-
-
-
-    while (num <= magicBox * magicBox) {
-
-                                                                                    //print the number
-         //cout<<"row: "<<row<<"col: "<<col<<endl;
-
-        myTerm.terminalSleep(1);
-        currentColor=myTerm.getRandColor();
-        myTerm.displayText(currentColor,to_string(num),(row+1)*10,(col+1)*5);
-                                                        // If next cell is already filled, move down instead
-        if (num%magicBox==0) {
-            row = (row + 1) % magicBox;                 // Move down from original position
-                                                        // col stays the same
-        } else {
-            row = (row - 1 + magicBox) % magicBox; // Move up (with wrap)
-            col = (col + 1) % magicBox;             // Move right (with wrap)
-        }
-
-
-       num++;
-    }
-    int width=DEFAULT_VAL,height=DEFAULT_VAL;
-
-    myTerm.getTerminalSize(width,height);
-    currentColor=myTerm.getRandColor();
-    myTerm.displayText(currentColor,"",width+1,height);
-
+    char a[10]="ANdREw";
+    char b[10]="";
+    cout<<str_copy(a,b,10)<<endl;
+    printf("%s",b);
 
 
     return 0;
