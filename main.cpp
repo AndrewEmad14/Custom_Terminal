7266d2d15c6888b4b2d11c0af95efbc01dcb319f
Andrew Emad <andrew14emad@gmail.com>
2025-11-19 19:09:23 +0200

    i swapped

--- main.cpp
+++ main.cpp
@@ -1,71 +1,30 @@
 #include <iostream>
-#include "TerminalModifier.h"
-#define DEFAULT_VAL 0
-#define STARTNUM 1
-#define STARTROW 0
-#define PADDING 5
-#define OFFSET 10
-#define DEFAULT_STRING_VAL ""
-using namespace std;
 
+using namespace std;
+void swapUsingPlus(int *x,int *y){
+
+    *x=*x+*y;
+    *y=*x-*y;
+    *x=*x-*y;
+}
+void swapUsingMultiply(int *x,int *y){
+
+        *x=(*x)*(*y);
+        *y=(*x)/ (*y);
+        *x=(*x)/ (*y);
+
+}
+void swapUsingXOR(int *x,int *y){
+        *x=(*x)^(*y);
+        *y=(*x)^(*y);
+        *x=(*x)^(*y);
+}
 
 int main(void) {
-    TerminalModifier myTerm;
-
-
-    int magicBox=DEFAULT_VAL;
-    string currentColor= DEFAULT_STRING_VAL;
-    currentColor=myTerm.getRandColor();
-    int exit=DEFAULT_VAL;
-    while(!exit){
-          myTerm.displayText(YELLOW_COLOR,"Please choose an odd number:",0,0);
-          cin>>magicBox;
-          if(magicBox%2==0){
-                myTerm.displayText(MAGENTA_COLOR,"this isnt an odd number, please choose an odd one",0,0);
-                myTerm.terminalSleep(1);
-                myTerm.clearScreen();
-          }else{
-              exit=1;
-          }
-
-
-
-    }
-
-
-    int num = STARTNUM;
-    int row = STARTROW;                  // Start at top row
-    int col = magicBox / 2;              // Middle column
-
-
-
-    while (num <= magicBox * magicBox) {
-
-                                                                                    //print the number
-         //cout<<"row: "<<row<<"col: "<<col<<endl;
-
-        myTerm.terminalSleep(1);
-        currentColor=myTerm.getRandColor();
-        myTerm.displayText(currentColor,to_string(num),(row+1)*10,(col+1)*5);
-                                                        // If next cell is already filled, move down instead
-        if (num%magicBox==0) {
-            row = (row + 1) % magicBox;                 // Move down from original position
-                                                        // col stays the same
-        } else {
-            row = (row - 1 + magicBox) % magicBox; // Move up (with wrap)
-            col = (col + 1) % magicBox;             // Move right (with wrap)
-        }
-
-
-       num++;
-    }
-    int width=DEFAULT_VAL,height=DEFAULT_VAL;
-
-    myTerm.getTerminalSize(width,height);
-    currentColor=myTerm.getRandColor();
-    myTerm.displayText(currentColor,"",width+1,height);
-
-
-
+    int x=10,y=5;
+    swapUsingPlus(&x,&y);
+    swapUsingXOR(&x,&y);
+    swapUsingXOR(&x,&y);
+    cout<<"X: "<<x<<" Y: "<<y<<endl;
     return 0;
 }
